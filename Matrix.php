1be949158165b9e6aada35f9c5f5aff6050d9c6a
Nepster <nepsterxxx@mail.ru>
2015-01-11 14:41:48 +0200

    init

---
+++ Matrix.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace nepster\matrix;
+
+use Yii;
+
+
+/**
+ * Class Matrix
+ * @package nepster\matrix
+ */
+class Matrix extends \yii\base\Widget
+{
+
+    public function test()
+    {
+        return 'success';
+    }
+}
